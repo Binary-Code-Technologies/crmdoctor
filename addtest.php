172a7f5352bf31b3b1a880763f9c06304474b6c9
Priti-parida <pritiparida109@gmail.com>
2022-03-07 22:40:16 +0530

    addtable and taddtest.php

---
+++ addtest.php
@@ -0,0 +1,22 @@
+<?php
+
+include( 'conn.php');
+
+
+if(isset($_POST['addtest'])){
+print_r($_POST);
+//$test_name=mysqli_real_escape_string($conn,$_POST['test_id']);
+//$charge=mysqli_real_escape_string($conn,$_POST['charge']);
+//
+//$qty=mysqli_real_escape_string($conn,$_POST['qty']);
+//$total=mysqli_real_escape_string($conn,$_POST['total']);
+//
+//
+//$query = "INSERT INTO `test_entry`(`test_name`, `charge`,  `qty`, `total`) VALUES ('$test_name','$charge','$qty','$total')";
+//mysqli_query($conn,$query);
+//header('location:therapy_entry.php');
+}
+
+
+
+?>
